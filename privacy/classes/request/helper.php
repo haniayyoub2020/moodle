eefd5721374075e75f98ffe2f4d396d800938ebc
Andrew Nicols <andrew@nicols.co.uk>
2018-02-26 13:43:52 +0800

    MDL-61307 privacy: Extend unit tests for helper

--- privacy/classes/request/helper.php
+++ privacy/classes/request/helper.php
@@ -29,6 +29,14 @@ use \core_privacy\request\writer;
 require_once($CFG->libdir . '/modinfolib.php');
 require_once($CFG->dirroot . '/course/modlib.php');
 
+/**
+ * The core_privacy\request\helper class with useful shared functionality.
+ *
+ * @package core_privacy
+ * @copyright 2018 Andrew Nicols <andrew@nicols.co.uk>
+ *
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 class helper {
     /**
      * Get all general data for this context.
@@ -72,11 +80,6 @@ class helper {
         if (plugin_supports('mod', $cm->modname, FEATURE_MOD_INTRO, true)) {
             $intro = $moduledata->intro;
 
-            // Add the intro, name, and others.
-            writer::with_context($context)
-                // Export the files for the intro.
-                ->export_area_files([], $component, 'intro', 0);
-
             $intro = writer::with_context($context)
                 ->rewrite_pluginfile_urls([], $component, 'intro', 0, $intro);
 
@@ -111,14 +114,25 @@ class helper {
      */
     public static function export_context_files(\context $context, \stdClass $user) {
         if ($context instanceof \context_module) {
-            $coursecontext = $context->get_course_context();
-            $modinfo = get_fast_modinfo($coursecontext->instanceid);
-            $cm = $modinfo->cms[$context->instanceid];
-            $component = "mod_{$cm->modname}";
-
-            writer::with_context($context)
-                // Export the files for the intro.
-                ->export_area_files([], $component, 'intro', 0);
+            return static::export_context_module_files($context, $user);
         }
+    }
+
+    /**
+     * Get all general data for the activity module at this context.
+     *
+     * @param   \context_module $context The context to retrieve data for.
+     * @param   \stdClass       $user The user being written.
+     * @return  \stdClass
+     */
+    protected static function export_context_module_files(\context_module $context, \stdClass $user) {
+        $coursecontext = $context->get_course_context();
+        $modinfo = get_fast_modinfo($coursecontext->instanceid);
+        $cm = $modinfo->cms[$context->instanceid];
+        $component = "mod_{$cm->modname}";
+
+        writer::with_context($context)
+            // Export the files for the intro.
+            ->export_area_files([], $component, 'intro', 0);
     }
 }
--- privacy/tests/request_helper_test.php
+++ privacy/tests/request_helper_test.php
@@ -28,6 +28,7 @@ defined('MOODLE_INTERNAL') || die();
 global $CFG;
 
 use \core_privacy\request\helper;
+use \core_privacy\request\writer;
 
 /**
  * Tests for the \core_privacy API's request helper functionality.
@@ -37,58 +38,126 @@ use \core_privacy\request\helper;
  */
 class request_helper_test extends advanced_testcase {
     /**
-     * Test that module data is exported.
-     * TODO
+     * Test that basic module data is returned.
      */
     public function test_get_context_data_context_module() {
+        $this->resetAfterTest();
+
+        // Setup.
+        $course = $this->getDataGenerator()->create_course();
+        $user = \core_user::get_user_by_username('admin');
+
+        $forum = $this->getDataGenerator()->create_module('forum', [
+                'course' => $course->id,
+            ]);
+        $context = context_module::instance($forum->cmid);
+        $modinfo = get_fast_modinfo($course->id);
+        $cm = $modinfo->cms[$context->instanceid];
+
+        // Fetch the data.
+        $result = helper::get_context_data($context, $user);
+        $this->assertInstanceOf('stdClass', $result);
+
+        // Check that the name matches.
+        $this->assertEquals($cm->get_formatted_name(), $result->name);
+
+        // This plugin supports the intro. Check that it is included and correct.
+        $formattedintro = format_text($forum->intro, $forum->introformat, [
+                'noclean' => true,
+                'para' => false,
+                'context' => $context,
+                'overflowdiv' => true,
+            ]);
+        $this->assertEquals($formattedintro, $result->intro);
+
+        // This function should only fetch data. It does not export it.
+        $this->assertFalse(writer::with_context($context)->has_any_data());
     }
 
     /**
-     * Test that block data is exported.
+     * Test that a course moudle with completion tracking enabled has the completion data returned.
+     * TODO
+     */
+    public function test_get_context_data_context_module_completion() {
+    }
+
+    /**
+     * Test that block data is returned.
      * TODO
      */
     public function test_get_context_data_context_block() {
     }
 
     /**
-     * Test that course data is exported.
+     * Test that course data is returned.
      * TODO
      */
-    public function test_get_context_data_context_course() {
+    public function test_get_context_data_context_course_basic() {
     }
 
     /**
-     * Test that coursecat data is exported.
+     * Test that coursecat data is returned.
      * TODO
      */
     public function test_get_context_data_context_coursecat() {
     }
 
     /**
-     * Test that module data is exported.
-     * TODO
+     * Test that when there are no files to export for a course module context, nothing is exported.
      */
-    public function test_export_context_files_context_module() {
+    public function test_export_context_files_context_module_no_files() {
+        $this->resetAfterTest();
+
+        // Setup.
+        $course = $this->getDataGenerator()->create_course();
+        $user = \core_user::get_user_by_username('admin');
+
+        $forum = $this->getDataGenerator()->create_module('forum', [
+                'course' => $course->id,
+            ]);
+        $context = context_module::instance($forum->cmid);
+        $modinfo = get_fast_modinfo($course->id);
+        $cm = $modinfo->cms[$context->instanceid];
+
+        // Fetch the data.
+        helper::export_context_files($context, $user);
+
+        // This function should only fetch data. It does not export it.
+        $this->assertFalse(writer::with_context($context)->has_any_data());
+
     }
 
     /**
-     * Test that block data is exported.
+     * Test that when there are no files to export for a block context, nothing is exported.
      * TODO
      */
-    public function test_export_context_files_context_block() {
+    public function test_export_context_files_context_block_no_files() {
     }
 
     /**
-     * Test that course data is exported.
-     * TODO
+     * Test that when there are no files to export for a course context, nothing is exported.
      */
-    public function test_export_context_files_context_course() {
+    public function test_export_context_files_context_course_no_files() {
+        $this->resetAfterTest();
+
+        // Setup.
+        $course = $this->getDataGenerator()->create_course();
+        $user = \core_user::get_user_by_username('admin');
+        $context = context_course::instance($course->id);
+
+        // Fetch the data.
+        helper::export_context_files($context, $user);
+
+        // This function should only fetch data. It does not export it.
+        $this->assertFalse(writer::with_context($context)->has_any_data());
+
+
     }
 
     /**
-     * Test that coursecat data is exported.
+     * Test that when there are no files to export for a coursecat context, nothing is exported.
      * TODO
      */
-    public function test_export_context_files_context_coursecat() {
+    public function test_export_context_files_context_coursecat_no_files() {
     }
 }
