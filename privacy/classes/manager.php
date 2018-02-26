6fbd08da5b1f8aebe399051a10eafc8dca34b134
Jake Dallimore <jake@moodle.com>
2018-02-26 12:55:39 +0800

    MDL-61307 core_privacy: fleshing out the core_privacy\manager class

--- privacy/classes/manager.php
+++ privacy/classes/manager.php
@@ -19,17 +19,301 @@
  *
  * @package core_privacy
  * @copyright 2018 Jake Dallimore <jrhdallimore@gmail.com>
- *
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 namespace core_privacy;
+use core_privacy\metadata\item_collection;
+use core_privacy\phpunit\approved_contextlist;
+use core_privacy\request\contextlist_collection;
+use core_privacy\request\deletion_criteria;
 
 defined('MOODLE_INTERNAL') || die();
 
 /**
- * Class manager.
- * Provides the mechanisms to get and delete personal information across Moodle.
- * @package core_privacy
+ * The core_privacy\manager class, providing a facade to describe, export and delete personal data across Moodle and its components.
+ *
+ * This class is responsible for communicating with and collating privacy data from all relevant components, where relevance is
+ * determined through implementations of specific marker interfaces. These marker interfaces describe the responsibilities (in terms
+ * of personal data storage) as well as the relationship between the component and the core_privacy subsystem.
+ *
+ * The interface hierarchy is as follows:
+ * ├── metadata\null_provider
+ * ├── metadata\provider
+ * ├── request\data_provider
+ *     └── request\core_data_provider
+ *         └── request\core_user_data_provider
+ *             └── request\plugin\provider
+ *             └── request\subsystem\provider
+ *         └── request\user_preference_provider
+ *     └── request\shared_data_provider
+ *         └── request\plugin\subsystem_provider
+ *         └── request\plugin\subplugin_provider
+ *         └── request\subsystem\plugin_provider
+ *
+ * Describing personal data:
+ * -------------------------
+ * All components must state whether they store personal data (and DESCRIBE it) by implementing one of the metadata providers:
+ * - metadata\null_provider (indicating they don't store personal data)
+ * - metadata\provider (indicating they do store personal data, and describing it)
+ *
+ * The manager requests metadata for all Moodle components implementing the metadata\provider interface.
+ *
+ * Export and deletion of personal data:
+ * -------------------------------------
+ * Those components storing personal data need to provide EXPORT and DELETION of this data by implementing a request provider.
+ * Which provider implementation depends on the nature of the component; whether it's a sub-component and which components it
+ * stores data for.
+ *
+ * Export and deletion for sub-components (or any component storing data on behalf of another component) is managed by the parent
+ * component. If a component contains sub-components, it must ask those sub-components to provide the relevant data. Only certain
+ * 'core provider' components are called directly from the manager and these must provide the personal data stored by both
+ * themselves, and by all sub-components. Because of this hierarchical structure, the core_privacy\manager needs to know which
+ * components are to be called directly by core: these are called core data providers. The providers implemented by sub-components
+ * are called shared data providers.
+ *
+ * The following are interfaces are not implemented directly, but are marker interfaces uses to classify components by nature:
+ * - request\data_provider:
+ *      Not implemented directly. Used to classify components storing personal data of some kind. Includes both components storing
+ *      personal data for themselves and on behalf of other components.
+ *      Include: request\core_data_provider and request\shared_data_provider.
+ * - request\core_data_provider:
+ *      Not implemented directly. Used to classify components storing personal data for themselves and which are to be called by the
+ *      core_privacy subsystem directly.
+ *      Includes: request\core_user_data_provider and request\user_preference_provider.
+ * - request\core_user_data_provider:
+ *      Not implemented directly. Used to classify components storing personal data for themselves, which are either a plugin or
+ *      subsystem and which are to be called by the core_privacy subsystem directly.
+ *      Includes: request\plugin\provider and request\subsystem\provider.
+ * - request\shared_data_provider:
+ *      Not implemented directly. Used to classify components storing personal data on behalf of other components and which are
+ *      called by the owning component directly.
+ *      Includes: request\plugin\subsystem_provider, request\plugin\subplugin_provider and request\subsystem\plugin_provider
+ *
+ * The manager only requests the export or deletion of personal data for components implementing the request\core_data_provider
+ * interface or one of its descendants; request\plugin\provider, request\subsystem\provider or request\user_preference_provider.
+ * Implementing one of these signals to the core_privacy subsystem that the component must be queried directly from the manager.
+ *
+ * Any component using another component to store personal data on its behalf, is responsible for making the relevant call to
+ * that component's relevant shared_data_provider class.
+ *
+ * For example:
+ * The manager calls a core_data_provider component (e.g. mod_assign) which, in turn, calls relevant subplugins or subsystems
+ * (which assign uses to store personal data) to get that data. All data for assign and its sub-components is aggregated by assign
+ * and returned to the core_privacy subsystem.
+ *
+ * @copyright 2018 Jake Dallimore <jrhdallimore@gmail.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class manager {
+    /**
+     * Checks whether the given component is compliant with the core_privacy API.
+     * To be considered compliant, a component must declare whether (and where) it stores personal data.
+     *
+     * Components which do store personal data must:
+     * - Have implemented the core_privacy\metadata\provider interface (to describe the data it stores) and;
+     * - Have implemented the core_privacy\request\data_provider interface (to facilitate export of personal data)
+     * - Have implemented the core_privacy\request\deleter interface
+     *
+     * Components which do not store personal data must:
+     * - Have implemented the core_privacy\metadata\null_provider interface to signal that they don't store personal data.
+     *
+     * @param string $component frankenstyle component name, e.g. 'mod_assign'
+     * @return bool true if the component is compliant, false otherwise.
+     */
+    public function component_is_compliant(string $component) : bool {
+        // Components which don't store user data need only implement the null_provider.
+        if ($this->component_implements($component, \core_privacy\metadata\null_provider::class)) {
+            return true;
+        }
+        // Components which store user data must implement the metadata\provider and the request\data_provider.
+        if ($this->component_implements($component, \core_privacy\metadata\provider::class) &&
+            $this->component_implements($component, \core_privacy\request\data_provider::class)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Get the privacy metadata for all components.
+     *
+     * @return item_collection[] The array of item_collection objects, indexed by frankenstyle component name.
+     */
+    public function get_metadata_for_components() : array {
+        // Get the metadata, and put into an assoc array indexed by component name.
+        $metadata = [];
+        foreach ($this->get_component_list() as $component) {
+            if ($this->component_implements($component, \core_privacy\metadata\provider::class)) {
+                $metadata[$component] = $this->get_provider_classname($component)::get_metadata(new item_collection($component));
+            }
+        }
+        return $metadata;
+    }
+
+    /**
+     * Gets a collection of resultset objects for all components.
+     *
+     * @param int $userid the id of the user we're fetching contexts for.
+     * @return contextlist_collection the collection of contextlist items for the respective components.
+     */
+    public function get_contexts_for_userid(int $userid) : contextlist_collection {
+        $clcollection = new contextlist_collection();
+        foreach ($this->get_core_provider_components() as $component) {
+            $contextlist = $this->get_provider_classname($component)::get_contexts_for_userid($userid);
+            $contextlist->set_component($component); // Each contextlist is tied to its respective component.
+            $clcollection->add_contextlist($contextlist);
+        }
+        return $clcollection;
+    }
+
+    /**
+     * Export all user data for the specified approved_contextlist items.
+     *
+     * Note: userid and component are stored in each respective approved_contextlist.
+     *
+     * @param contextlist_collection $contextlistcollection the collection of contextlists for all components.
+     * @return string the location of the exported data.
+     * @throws \moodle_exception if the contextlist_collection does not contain all approved_contextlist items or if one of the
+     * approved_contextlists' components is not a core_data_provider.
+     */
+    public function export_user_data(contextlist_collection $contextlistcollection) {
+        $approvedcontextlists = $contextlistcollection->get_contextlists();
+        // Sanity checks.
+        foreach ($approvedcontextlists as $approvedcontextlist) {
+            if (!$approvedcontextlist instanceof \core_privacy\request\approved_contextlist) {
+                throw new \moodle_exception('Contextlist must be an approved_contextlist');
+            }
+
+            // The component with which the contextlist is associated must be a core_data_provider of sorts.
+            $component = $approvedcontextlist->get_component();
+            if (!$this->component_is_core_provider($component)) {
+                throw new \moodle_exception("Component '$component' must be a core data provider");
+            }
+        }
+        // Export for the various components/contexts.
+        foreach($approvedcontextlists as $approvedcontextlist) {
+            $component = $approvedcontextlist->get_component();
+            // Core user data providers.
+            if ($this->component_implements($component, \core_privacy\request\core_user_data_provider::class)) {
+                $this->get_provider_classname($component)::export_user_data($approvedcontextlist);
+            }
+            // Core user preference providers.
+            if ($this->component_implements($component, \core_privacy\request\user_preference_provider::class)) {
+                $this->get_provider_classname($component)::export_user_preferences($approvedcontextlist);
+            }
+        }
+        return request\writer::with_context(\context_system::instance())->finalise_content();
+    }
+
+    /**
+     * Delete all user data for approved contexts lists provided in the collection.
+     *
+     * Note: userid and component are stored in each respective approved_contextlist.
+     *
+     * @param contextlist_collection $contextlistcollection the collections of approved_contextlist items on which to call deletion.
+     * @throws \moodle_exception if the contextlist_collection doesn't contain all approved_contextlist items, or if the component
+     * for an approved_contextlist isn't a core provider.
+     */
+    public function delete_user_data(contextlist_collection $contextlistcollection) {
+        $approvedcontextlists = $contextlistcollection->get_contextlists();
+        // Sanity checks.
+        foreach ($approvedcontextlists as $approvedcontextlist) {
+            if (!$approvedcontextlist instanceof \core_privacy\request\approved_contextlist) {
+                throw new \moodle_exception('Contextlist must be an approved_contextlist');
+            }
+            if (!$this->component_is_core_provider($approvedcontextlist->get_component())) {
+                throw new \moodle_exception('Component must be a core provider');
+            }
+        }
+        // Delete the data.
+        foreach($approvedcontextlists as $approvedcontextlist) {
+            $this->get_provider_classname($approvedcontextlist->get_component())::delete_user_data($approvedcontextlist);
+        }
+    }
+
+    /**
+     * Delete user data for all users using the specified deletion_criteria.
+     *
+     * @param deletion_criteria $criteria the criteria object dictating what contexts will be deleted.
+     */
+    public function delete_for_context(deletion_criteria $criteria) {
+        foreach ($this->get_core_provider_components() as $component) {
+            $this->get_provider_classname($component)::delete_for_context($criteria);
+        }
+    }
+
+    /**
+     * Check whether the specified component is a core provider.
+     *
+     * @param string $component the frankenstyle component name.
+     * @return bool true if the component is a core provider, false otherwise.
+     */
+    protected function component_is_core_provider($component) {
+        return $this->component_implements($component, \core_privacy\request\core_data_provider::class);
+    }
+
+    /**
+     * Returns all the components which are core providers.
+     *
+     * @return array the array of frankenstyle core provider component names.
+     */
+    protected function get_core_provider_components() {
+        $cpcomponents = [];
+        foreach ($this->get_component_list() as $component) {
+            if ($this->component_implements($component, \core_privacy\request\core_data_provider::class)) {
+                $cpcomponents[] = $component;
+            }
+        }
+        return $cpcomponents;
+    }
+
+    /**
+     * Returns a list of frankenstyle names of core components (plugins and subsystems).
+     *
+     * @return array the array of frankenstyle component names.
+     */
+    protected function get_component_list() {
+        $components = [];
+        // Get all plugins.
+        $plugintypes = \core_component::get_plugin_types();
+        foreach ($plugintypes as $plugintype => $typedir) {
+            $plugins = \core_component::get_plugin_list($plugintype);
+            foreach ($plugins as $pluginname => $plugindir) {
+                $components[] = $plugintype . '_' . $pluginname;
+            }
+        }
+        // Get all subsystems.
+        foreach (\core_component::get_core_subsystems() as $name => $path) {
+            if (isset($path)) {
+                $components[] = 'core_' . $name;
+            }
+        }
+        return $components;
+    }
+
+    /**
+     * Return the fully qualified provider classname for the component.
+     *
+     * @param string $component the frankenstyle component name.
+     * @return string the fully qualified provider classname.
+     */
+    protected function get_provider_classname($component) {
+        return "$component\privacy\provider";
+    }
+
+    /**
+     * Checks whether the component's provider class implements the specified interface.
+     * This can either be implemented directly, or by implementing a descendant (extension) of the specified interface.
+     *
+     * @param string $component the frankenstyle component name.
+     * @param string $interface the name of the interface we want to check.
+     * @return bool True if an implementation was found, false otherwise.
+     */
+    protected function component_implements(string $component, string $interface) : bool {
+        $providerclass = $this->get_provider_classname($component);
+        if (class_exists($providerclass)) {
+            $rc = new \ReflectionClass($providerclass);
+            return $rc->implementsInterface($interface);
+        }
+        return false;
+    }
 }
