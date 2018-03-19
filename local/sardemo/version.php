108811e78ba367a4202598eef2cffee217c0a4c9
Andrew Nicols <andrew@nicols.co.uk>
2018-03-19 11:13:53 +0800

    local_sardemo: SAR Demo tool to reset user tour states for the logged in user

---
+++ local/sardemo/version.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * SAR Demo helper.
+ *
+ * @package     local_sardemo
+ * @copyright   2018 Andrew Nicols <andrew@nicols.co.uk>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+$plugin->version   = 2018031002;
+$plugin->requires  = 2017111301.05;        // TODO: This actually requires Moodle 3.4.2 (Build 2018031200) and upwards.
+$plugin->component = 'local_sardemo';
+$plugin->release   = '0.0.1';
+$plugin->maturity  = MATURITY_ALPHA;
