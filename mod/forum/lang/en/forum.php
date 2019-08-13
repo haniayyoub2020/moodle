5b891e3813eb4332badb853ed4ca67240a2d6db2
Mathew May <mathewm@hotmail.co.nz>
2019-08-13 14:13:17 +0800

    MDL-66080 mod_forum: relocate templates into forum
    Part of MDL-66074

--- mod/forum/lang/en/forum.php
+++ mod/forum/lang/en/forum.php
@@ -704,6 +704,7 @@ $string['forumsubjectdeleted'] = 'This forum post has been removed';
 $string['forumbodydeleted'] = 'The content of this forum post has been removed and can no longer be accessed.';
 $string['gradeusers'] = 'Grade users';
 $string['forumgrader'] = 'Forum grader';
+$string['grading'] = 'Grading';
 
 // Deprecated since Moodle 3.8.
 $string['inpagereplysubject'] = 'Re: {$a}';
