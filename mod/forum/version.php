b607dfde59930b678b61d2a31bc2d2c74a889f77
Andrew Nicols <andrew@nicols.co.uk>
2019-09-18 10:54:26 +0800

    MDL-66696 forum: Add forum_grades table

--- mod/forum/version.php
+++ mod/forum/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019081100;       // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2019091700;       // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2019051100;       // Requires this Moodle version
 $plugin->component = 'mod_forum';      // Full name of the plugin (used for diagnostics)
