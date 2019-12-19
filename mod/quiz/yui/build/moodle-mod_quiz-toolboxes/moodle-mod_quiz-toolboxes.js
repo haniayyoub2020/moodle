54ff7b70db3c3a48288ed569b2d1b372977b5929
Andrew Nicols <andrew@nicols.co.uk>
2019-12-19 13:28:47 +0800

    MDL-67658 quiz: Add pending promises for various behat steps

--- mod/quiz/yui/build/moodle-mod_quiz-toolboxes/moodle-mod_quiz-toolboxes.js
+++ mod/quiz/yui/build/moodle-mod_quiz-toolboxes/moodle-mod_quiz-toolboxes.js
@@ -357,6 +357,7 @@ Y.extend(RESOURCETOOLBOX, TOOLBOX, {
      * @returns {boolean}
      */
     handle_data_action: function(ev) {
+        M.util.js_pending('mod_quiz/toolbox/resource:handle_data_action');
         // We need to get the anchor element that triggered this event.
         var node = ev.target;
         if (!node.test('a')) {
@@ -370,6 +371,7 @@ Y.extend(RESOURCETOOLBOX, TOOLBOX, {
 
         if (!node.test('a') || !action || !activity) {
             // It wasn't a valid action node.
+            M.util.js_complete('mod_quiz/toolbox/resource:handle_data_action');
             return;
         }
 
@@ -397,6 +399,7 @@ Y.extend(RESOURCETOOLBOX, TOOLBOX, {
                 // Nothing to do here!
                 break;
         }
+        M.util.js_complete('mod_quiz/toolbox/resource:handle_data_action');
     },
 
     /**
@@ -442,6 +445,7 @@ Y.extend(RESOURCETOOLBOX, TOOLBOX, {
      * @param {Node} activity The activity node that this action will be performed on.
      */
     delete_with_confirmation: function(ev, button, activity) {
+        M.util.js_pending('mod_quiz/toolbox/resource:delete_with_confirmation');
         // Prevent the default button action.
         ev.preventDefault();
 
@@ -479,6 +483,8 @@ Y.extend(RESOURCETOOLBOX, TOOLBOX, {
             });
 
         }, this);
+
+        M.util.js_complete('mod_quiz/toolbox/resource:delete_with_confirmation');
     },
 
     /**
@@ -517,6 +523,7 @@ Y.extend(RESOURCETOOLBOX, TOOLBOX, {
      * @param {EventFacade} ev The event that was fired.
      */
     delete_multiple_action: function(ev) {
+        M.util.js_pending('mod_quiz/toolbox/resource:delete_multiple_action');
         var problemsection = this.find_sections_that_would_become_empty();
 
         if (typeof problemsection !== 'undefined') {
@@ -529,6 +536,7 @@ Y.extend(RESOURCETOOLBOX, TOOLBOX, {
         } else {
             this.delete_multiple_with_confirmation(ev);
         }
+        M.util.js_complete('mod_quiz/toolbox/resource:delete_multiple_action');
     },
 
     /**
@@ -539,6 +547,7 @@ Y.extend(RESOURCETOOLBOX, TOOLBOX, {
      * @param {EventFacade} ev The event that was fired.
      */
     delete_multiple_with_confirmation: function(ev) {
+        M.util.js_pending('mod_quiz/toolbox/resource:delete_multiple_with_confirmation');
         ev.preventDefault();
 
         var ids = '';
@@ -553,6 +562,7 @@ Y.extend(RESOURCETOOLBOX, TOOLBOX, {
 
         // Do nothing if no slots are selected.
         if (!slots || !slots.length) {
+            M.util.js_complete('mod_quiz/toolbox/resource:delete_multiple_with_confirmation');
             return;
         }
 
@@ -587,6 +597,8 @@ Y.extend(RESOURCETOOLBOX, TOOLBOX, {
             });
 
         }, this);
+
+        M.util.js_complete('mod_quiz/toolbox/resource:delete_multiple_with_confirmation');
     },
 
     /**
@@ -601,6 +613,7 @@ Y.extend(RESOURCETOOLBOX, TOOLBOX, {
      * @return Boolean
      */
     edit_maxmark: function(ev, button, activity) {
+        M.util.js_pending('mod_quiz/toolbox/resource:edit_maxmark');
         // Get the element we're working on
         var instancemaxmark = activity.one(SELECTOR.INSTANCEMAXMARK),
             instance = activity.one(SELECTOR.ACTIVITYINSTANCE),
@@ -661,6 +674,8 @@ Y.extend(RESOURCETOOLBOX, TOOLBOX, {
             // Handle form submission.
             thisevent = editform.on('submit', this.edit_maxmark_submit, this, activity, oldmaxmark);
             this.editmaxmarkevents.push(thisevent);
+
+            M.util.js_complete('mod_quiz/toolbox/resource:edit_maxmark');
         });
     },
 
@@ -674,6 +689,7 @@ Y.extend(RESOURCETOOLBOX, TOOLBOX, {
      * @param {String} originalmaxmark The original maxmark the activity or resource had.
      */
     edit_maxmark_submit: function(ev, activity, originalmaxmark) {
+        M.util.js_pending('mod_quiz/toolbox/resource:edit_maxmark_submit');
         // We don't actually want to submit anything.
         ev.preventDefault();
         var newmaxmark = Y.Lang.trim(activity.one(SELECTOR.ACTIVITYFORM + ' ' + SELECTOR.ACTIVITYMAXMARK).get('value'));
@@ -691,6 +707,7 @@ Y.extend(RESOURCETOOLBOX, TOOLBOX, {
                 if (response.instancemaxmark) {
                     activity.one(SELECTOR.INSTANCEMAXMARK).setContent(response.instancemaxmark);
                 }
+                M.util.js_complete('mod_quiz/toolbox/resource:edit_maxmark_submit');
             });
         }
     },
@@ -719,6 +736,8 @@ Y.extend(RESOURCETOOLBOX, TOOLBOX, {
      * @param {Node} activity  The activity whose maxmark we were altering.
      */
     edit_maxmark_clear: function(activity) {
+        M.util.js_pending('mod_quiz/toolbox/resource:edit_maxmark_clear');
+
         // Detach all listen events to prevent duplicate triggers
         new Y.EventHandle(this.editmaxmarkevents).detach();
 
@@ -737,14 +756,8 @@ Y.extend(RESOURCETOOLBOX, TOOLBOX, {
         // Refocus the link which was clicked originally so the user can continue using keyboard nav.
         Y.later(100, this, function() {
             activity.one(SELECTOR.EDITMAXMARK).focus();
+            M.util.js_complete('mod_quiz/toolbox/resource:edit_maxmark_clear');
         });
-
-        // TODO MDL-50768 This hack is to keep Behat happy until they release a version of
-        // MinkSelenium2Driver that fixes
-        // https://github.com/Behat/MinkSelenium2Driver/issues/80.
-        if (!Y.one('input[name=maxmark')) {
-            Y.one('body').append('<input type="text" name="maxmark" style="display: none">');
-        }
     },
 
     /**
@@ -760,6 +773,7 @@ Y.extend(RESOURCETOOLBOX, TOOLBOX, {
      * @chainable
      */
     update_page_break: function(ev, button, activity, action) {
+        M.util.js_pending('mod_quiz/toolbox/resource:update_page_break');
         // Prevent the default button action
         ev.preventDefault();
 
@@ -784,6 +798,7 @@ Y.extend(RESOURCETOOLBOX, TOOLBOX, {
                 }
                 this.reorganise_edit_page();
             }
+            M.util.js_complete('mod_quiz/toolbox/resource:update_page_break');
         });
 
         return this;
@@ -802,6 +817,7 @@ Y.extend(RESOURCETOOLBOX, TOOLBOX, {
      * @chainable
      */
     update_dependency: function(ev, button, activity, action) {
+        M.util.js_pending('mod_quiz/toolbox/resource:update_dependency');
         // Prevent the default button action.
         ev.preventDefault();
         var spinner = this.add_spinner(activity);
@@ -817,6 +833,7 @@ Y.extend(RESOURCETOOLBOX, TOOLBOX, {
             if (response.hasOwnProperty('requireprevious')) {
                 Y.Moodle.mod_quiz.util.slot.updateDependencyIcon(activity, response.requireprevious);
             }
+            M.util.js_complete('mod_quiz/toolbox/resource:update_dependency');
         });
 
         return this;
