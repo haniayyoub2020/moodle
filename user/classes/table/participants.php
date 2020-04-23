1b35a22c10ddf8cb9d7937536f68f5f4bc4190fb
Andrew Nicols <andrew@nicols.co.uk>
2020-04-23 09:22:27 +0800

    MDL-68454 table: Move get_context from dynamic to flexible

--- user/classes/table/participants.php
+++ user/classes/table/participants.php
@@ -566,7 +566,7 @@ class participants extends \table_sql implements dynamic_table {
      *
      * @return context
      */
-    public function get_context(): ?context {
+    public function get_context(): context {
         return $this->context;
     }
 
