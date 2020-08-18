6a44e3dc0f0cbca3f580122ec036fde713dd78d8
Andrew Nicols <andrew@nicols.co.uk>
2020-08-18 11:13:01 +0800

    MDL-64874 travis: Restructure Travis to make it more manageable

---
+++ .travis/config.php
@@ -0,0 +1,43 @@
+<?php
+
+unset($CFG);
+global $CFG;
+
+// Basic configuration.
+$CFG = (object) [
+    'dbtype'        => '',
+    'dblibrary'     => 'native',
+    'dbhost'        => 'localhost',
+    'dbname'        => 'travis_ci_test',
+    'dbuser'        => '',
+    'dbpass'        => '',
+    'prefix'        => 'mdl_',
+    'dboptions'     => [
+        'dbcollation' => '',
+    ],
+    'wwwroot'       => 'https://localhost',
+    'dataroot'      => '/home/travis/roots/base',
+
+    // PHPUnit configuration.
+    'phpunit_dataroot' => '/home/travis/roots/phpunit',
+    'phpunit_prefix' => 'p_',
+];
+
+// Database-specific configuration.
+switch(getenv('DB')) {
+    case 'pgsql':
+        $CFG->dbtype = 'pgsql';
+        $CFG->dbuser = 'postgres';
+        $CFG->dboptions['dbcollation'] = 'utf8mb4_unicode_ci';
+        break;
+    case 'mysqli':
+        $CFG->dbtype = 'mysqli';
+        $CFG->dbuser =   'travis';
+        $CFG->dboptions['dbcollation'] = 'utf8mb4_bin';
+        break;
+}
+
+define('TEST_EXTERNAL_FILES_HTTP_URL', 'http://127.0.0.1:8080');
+define('TEST_CACHESTORE_REDIS_TESTSERVERS', '127.0.0.1');
+
+require_once(__DIR__ . '/lib/setup.php');
