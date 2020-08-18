a1294a4badadfc83f709fd380960e0f4aabec47b
Andrew Nicols <andrew@nicols.co.uk>
2020-08-18 11:13:01 +0800

    MDL-64874 travis: Restructure Travis to make it more manageable

---
+++ .travis/install.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+# Exit immediately upon failure.
+set -e
+
+# Add Travis functions.
+source "${TRAVIS_HOME}/.travis/functions"
+
+########################################################################
+# GRUNT Tests.
+########################################################################
+if [ "$TASK" = 'GRUNT' ];
+then
+    # Ensure that nvm functions are loaded.
+    source ~/.nvm/nvm.sh
+
+    # Install the NodeJS and NPM versions defined in the .nvmrc file.
+    nvm install
+    nvm use
+
+    # Install grunt globally.
+    npm install --no-spin -g grunt
+
+    # Install NPM dependencies.
+    npm install --no-spin
+fi
+
+
+########################################################################
+# PHPUNIT Tests.
+########################################################################
+if [ "$TASK" = 'PHPUNIT' ]
+then
+    if [ -n "$GITHUB_APITOKEN" ]
+    then
+        composer config github-oauth.github.com $GITHUB_APITOKEN
+        echo 'auth.json' >> .git/info/exclude
+    fi
+
+    # Enable Redis.
+    # Redis tests are currently failing on php 7.2 due to https://bugs.php.net/bug.php?id=75628
+    # echo 'extension="redis.so"' > /tmp/redis.ini
+    # phpenv config-add /tmp/redis.ini
+
+    # Install composer dependencies.
+    # We need --no-interaction in case we hit API limits for composer. This causes it to fall back to a standard clone.
+    # Typically it should be able to use the Composer cache if any other job has already completed before we started here.
+    travis_retry composer install --prefer-dist --no-interaction
+
+    # Initialise PHPUnit for Moodle.
+    php admin/tool/phpunit/cli/init.php
+fi
---
+++ .travis/script.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+# Exit immediately upon failure.
+set -e
+
+# Add Travis functions.
+source "${TRAVIS_HOME}/.travis/functions"
+
+########################################################################
+# PHPUNIT Tests.
+########################################################################
+if [ "$TASK" = 'PHPUNIT' ]
+then
+    vendor/bin/phpunit --fail-on-risky --disallow-test-output --verbose
+fi
+
+########################################################################
+# CITEST Tests.
+########################################################################
+if [ "$TASK" = 'CITEST' ]
+then
+    export GIT_PREVIOUS_COMMIT="`git merge-base FETCH_HEAD $TRAVIS_COMMIT`"
+    export GIT_COMMIT="$TRAVIS_COMMIT"
+    export UPSTREAM_FETCH_HEAD=`git rev-parse FETCH_HEAD`
+
+    # Variables required by our linter.
+    export gitcmd=`which git`
+    export gitdir="$TRAVIS_BUILD_DIR"
+    export phpcmd=`which php`
+
+    bash local/ci/php_lint/php_lint.sh
+fi
+
+########################################################################
+# GRUNT Tests.
+########################################################################
+if [ "$TASK" = 'GRUNT' ]
+then
+    source ~/.nvm/nvm.sh
+
+    # Use the correct version of node.
+    nvm use
+
+    # Run a full `grunt`
+    grunt
+
+    # Also run `grunt ignorefiles` which is not included in a standard run.
+    grunt ignorefiles
+
+    # Add all files to the git index and then run diff --cached to see all changes.
+    # This ensures that we get the status of all files, including new files.
+    # We ignore npm-shrinkwrap.json to make the tasks immune to npm changes.
+    git add .
+    git reset -- npm-shrinkwrap.json
+    git diff --cached --exit-code
+fi
+
+########################################################################
+# Upgrade test
+########################################################################
+if [ "$TASK" = 'UPGRADE' ]
+then
+    cp local/ci/check_upgrade_savepoints/check_upgrade_savepoints.php ./check_upgrade_savepoints.php
+    result=`php check_upgrade_savepoints.php`
+
+    # Check if there are problems
+    count=`echo "$result" | grep -P "ERROR|WARN" | wc -l`
+    if [ $count -gt 0 ]
+    then
+        echo "$result"
+        exit 1
+    fi
+fi
